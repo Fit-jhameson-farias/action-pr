73c129886becd8c8ed8ff04a7b600dd50137d712
Jhameson Lucas <85688136+Fit-jhameson-farias@users.noreply.github.com>
2022-08-16 15:44:56 -0300

    alterações

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,6 @@ then
   echo "Destination head branch cannot be 'main' nor 'master'"
   return -1
 fi
-if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
-then
-  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
-else
-  PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
-fi
 CLONE_DIR=$(mktemp -d)
 echo "Setting git variables"
 export GITHUB_TOKEN=$API_TOKEN_GITHUB
@@ -32,17 +26,14 @@ git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
 echo "Adding git commit"
 git add .
-if git status | grep -q "Changes to be committed"
-then
-  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-  echo "Pushing git commit"
-  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
-  echo "Creating a pull request"
-  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-               -b $INPUT_DESTINATION_HEAD_BRANCH \
-               -B $INPUT_DESTINATION_BASE_BRANCH \
-               -H $INPUT_DESTINATION_HEAD_BRANCH \
-                  $PULL_REQUEST_REVIEWERS
-else
-  echo "No changes detected"
-fi
+git status 
+git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+echo "Pushing git commit"
+git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+echo "Creating a pull request"
+gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+              -b $INPUT_DESTINATION_HEAD_BRANCH \
+              -B $INPUT_DESTINATION_BASE_BRANCH \
+              -H $INPUT_DESTINATION_HEAD_BRANCH 
+
+echo "No changes detected"

ca60600cfe58075f5c724c143b31e7fbd17b7189
Jhameson Lucas <85688136+Fit-jhameson-farias@users.noreply.github.com>
2022-08-17 09:29:04 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,20 +24,24 @@ git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
 echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
-
 echo "Copying contents to git repo"-r $INPUT_USER_NAME
 cp -R $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
 cd "$CLONE_DIR"
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
 echo "Adding git commit"
-
 git add .
-git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-git push origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
-echo "Creating a pull request"
-gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-              -b $INPUT_DESTINATION_HEAD_BRANCH \
-              -B $INPUT_DESTINATION_BASE_BRANCH \
-              -H $INPUT_DESTINATION_HEAD_BRANCH \
-                  $PULL_REQUEST_REVIEWERS            
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+  echo "Pushing git commit"
+  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+  echo "Creating a pull request"
+  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+               -b $INPUT_DESTINATION_HEAD_BRANCH \
+               -B $INPUT_DESTINATION_BASE_BRANCH \
+               -H $INPUT_DESTINATION_HEAD_BRANCH \
+                  $PULL_REQUEST_REVIEWERS
+else
+  echo "No changes detected"
+fi

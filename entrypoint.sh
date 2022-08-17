ce8210502fdb47d33972143d50a99dcf4c7446bc
fit-jhameson-farias <jhameson.farias@fitbank.com.br>
2022-08-17 10:39:38 -0300

    a

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
+
 set -e
 set -x
+
 if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
   echo "Source folder must be defined"
   return -1
 fi
+
 if [ $INPUT_DESTINATION_HEAD_BRANCH == "main" ] || [ $INPUT_DESTINATION_HEAD_BRANCH == "master"]
 then
   echo "Destination head branch cannot be 'main' nor 'master'"
   return -1
 fi
+
 if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
 then
   PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
 else
   PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
 fi
+
 CLONE_DIR=$(mktemp -d)
+
 echo "Setting git variables"
 export GITHUB_TOKEN=$API_TOKEN_GITHUB
 git config --global user.email "$INPUT_USER_EMAIL"
@@ -25,16 +31,12 @@ git config --global user.name "$INPUT_USER_NAME"
 
 echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+
+echo "Copying contents to git repo"
+mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
-
-# echo "Cloning base repository"
-# git clone -b main "https://$API_TOKEN_GITHUB@github.com/$INPUT_BASE_REPO.git" 
-
-cp -R $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
-git checkout --orphan "$INPUT_DESTINATION_HEAD_BRANCH"
-# git checkout --orphan gh-pages
-git rm -rf .
-git clean -fdx
+git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
 echo "Adding git commit"
 git add .
